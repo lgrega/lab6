933fad0f57a1b95dcd05dada78ed2095104d8120
lgrega <lisa.marie.grega@gmail.com>
2017-02-16 11:30:14 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,11 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, showProject);
+	console.log();
+
+	
 }
 
 /*
@@ -35,4 +40,27 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", changeColor);
+}
+
+function changeColor(result) {
+	console.log(result['colors'].hex);
+	var colors = result['colors'].hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+function showProject(result) {
+	console.log(result);
+
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' + 
+		'<h4>' + result['title'] + '</h4>' +
+		'<h5>' + result['date'] + '</h5>' +
+		'<p>' + result['summary'] + '</p>';
+
+	$(".project .details").html(projectHTML);
 }
